33f4336e518d98539843538c8e374c3f325c8af0
Hifaris <nattapongbe@gmail.com>
2025-02-19 14:09:23 +0700

    update: score to be variable

--- src/yihtzee/yahtzee.ts
+++ src/yihtzee/yahtzee.ts
@@ -2,21 +2,21 @@ type DiceNumber = 1 | 2 | 3 | 4 | 5 | 6;
 export type DiceRoll = [DiceNumber, DiceNumber, DiceNumber, DiceNumber, DiceNumber];
 
 class Yahtzee {
-  private score: number = 0;
+ 
 
   calculateDicesByTarget(dices: DiceRoll, target: number): number {
     if(target > 6 || target < 1) {
         throw new Error('Invalid target');
     }
 
-    this.score = 0;
+    let score:number = 0;
 
     for (let i: number = 0; i < dices.length; i++) {
         if (dices[i] === target) {
-            this.score += target;
+            score += target;
         }
     }
-    return this.score;
+    return score;
 }
 }
 
